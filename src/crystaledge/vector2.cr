dc55deff1180f43c53ecea9904785a178e9a8aa9
unn4m3d <smelnikov871@gmail.com>
2016-06-17 20:57:18 +1000

    Update vector2.cr

--- src/crystaledge/vector2.cr
+++ src/crystaledge/vector2.cr
@@ -1,6 +1,7 @@
 require "math"
 
 module CrystalEdge
+  #Representation of 2D vector
   class Vector2
     getter x,y
     setter x,y
@@ -16,73 +17,87 @@ module CrystalEdge
       return Vector2.new(0.0,0.0)
     end
 
-    # Dot product
+    # Returns dot product of two vectors
     def dot(other : Vector2)
       x*other.x + y*other.y
     end
 
-    # Cross product
+    # Returns cross product of two vectors
     def cross(other : Vector2)
       Vector2.new(self.x*other.y - self.y*other.x, self.y*other.x - self.x*other.y)
     end
 
+    # Alias for dot product
     def **(other : Vector2)
       dot other
     end
 
+    # Alias for cross product
     def %(other : Vector2)
       cross other
     end
 
+    # Returns magnitude of this vector
     def magnitude
       Math.sqrt(self.x**2 + self.y**2)
     end
 
+    # Returns angle between two vectors
     def angle(other : Vector2)
       self**other / (self.magnitude * other.magnitude)
     end
 
+    # Performs component addition
     def +(other : Vector2)
       Vector2.new(self.x+other.x,self.y+other.y)
     end
 
+    # Performs component addition
     def +(other : Float64)
       Vector2.new(self.x+other,self.y+other)
     end
 
+    # Performs component subtraction
     def -(other : Vector2)
       Vector2.new(self.x-other.x,self.y-other.y)
     end
 
+    # Performs component subtraction
     def -(other : Float64)
       Vector2.new(self.x-other,self.y-other)
     end
 
+    # Returns negated vector
     def -
       Vector2.new(-self.x,-self.y)
     end
 
+    # Performs component multiplication (for dot product see `#dot`)
     def *(other : Vector2)
       Vector2.new(self.x*other.x,self.y*other.y)
     end
 
+    # Performs multiplication
     def *(other : Float64)
       Vector2.new(self.x*other, self.y * other)
     end
-
+    # Performs component division
     def /(other : Vector2)
       Vector2.new(self.x/other.x,self.y/other.y)
     end
 
+    # Performs division
     def /(other : Float64)
       Vector2.new(self.x/other,self.y/other)
     end
 
+    # Clones this vector and passes it into a block if given
     def clone(&b)
       yield clone if block_given?
       Vector2.new(self.x,self.y)
     end
 
+    # Normalizes current vector
     def normalize!
       m = magnitude
       unless m == 0
@@ -92,6 +107,7 @@ module CrystalEdge
       self
     end
 
+    # Non-aggressive version of `#normalize!`
     def normalize
       if m == 0
         self
@@ -100,24 +116,25 @@ module CrystalEdge
       end
     end
 
+    # Finds normal axis between two vectors
     def find_normal_axis(other : Vector2)
       (self%other).normalize
     end
 
+    # Finds distance between two vectors
     def distance(other : Vector2)
       return (self-other).magnitude
     end
 
-    #TODO : Add multipying by matrix
-
     def ==(other : Vector2)
-      self.x == other.x && self.y == other.y #TODO : Comparsion with EPSILON
+      self.x == other.x && self.y == other.y
     end
 
     def !=(other : Vector2)
-      self.x != other.x || self.y != other.y #TODO : Comparsion with EPSILON
+      self.x != other.x || self.y != other.y
     end
 
+    # Formats vector
     def to_s
       "{X : #{x}; Y : #{y}}"
     end
